63936bc0cba2757c86415261bccd62be7dfaf00e
and_res_4 <huhdnuhdl@gmail.com>
2026-01-16 15:31:29 -0500

    Actualizar fxmanifest.lua

--- rv-npc-system./fxmanifest.lua
+++ rv-npc-system./fxmanifest.lua
@@ -1,1 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Andres - Rockstar Valle'
+description 'Sistema de NPCs Autónomos con Memoria y Economía'
+
+-- Importante: Declaramos oxmysql para la persistencia
+dependencies {
+    'oxmysql',
+    'ox_inventory' -- O el inventario que uses
+}
+
+client_scripts {
+    'config.lua',
+    'client/cl_main.lua',
+    'client/cl_driving.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/sv_main.lua'
+}
 
