d2c91ef3fc6618cb05f3a6b36ffb3b7095c07a09
and_res_4 <huhdnuhdl@gmail.com>
2026-01-16 15:35:55 -0500

    Actualizar config.lua

--- rv-npc-system./config.lua
+++ rv-npc-system./config.lua
@@ -1,1 +1,11 @@
 
+Config = {}
+
+-- Puntos lógicos: Puertas de tiendas, casas, callejones
+Config.SpawnPoints = {
+    {coords = vector3(24.1, -1106.8, 29.1), heading = 160.0, label = "Tienda de Ropa"},
+    {coords = vector3(-707.3, -914.5, 19.2), heading = 90.0, label = "Apartamento"},
+    {coords = vector3(1135.6, -982.2, 46.4), heading = 270.0, label = "Taller Mecánico"}
+}
+
+Config.MaxNPCsPorZona = 5 -- Para no saturar la CPU
